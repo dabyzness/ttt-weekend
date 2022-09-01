a14726d4f20d2f04771fe1acf66ef44f4ad9de1e
Damian <bzdyrad@gmail.com>
2022-09-01 16:58:11 -0400

    implement init

--- js/app.js
+++ js/app.js
@@ -5,8 +5,19 @@ let board, turn, winner;
 
 /*---------- Cached Element References ------------*/
 const sqaureEls = document.querySelectorAll("section > div");
-const messageEl = document.querySelector("#message");
+const messageEl = document.getElementById("message");
 
 /*--------------- Event Listeners --------*/
 
 /*------------------- Functions ------------------*/
+init();
+
+function init() {
+  board = new Array(9).fill(null);
+  turn = 1;
+  winner = null;
+
+  render();
+}
+
+function render() {}
