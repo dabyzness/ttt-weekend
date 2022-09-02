99a061f8e733085f28cd36a12be6af2ab0a34963
Damian <bzdyrad@gmail.com>
2022-09-02 10:35:13 -0400

    implement render function

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ let board, turn, winner;
 /*---------- Cached Element References ------------*/
 const sqaureEls = document.querySelectorAll("section > div");
 const messageEl = document.getElementById("message");
+console.log(sqaureEls);
 
 /*--------------- Event Listeners --------*/
 
@@ -20,4 +21,30 @@ function init() {
   render();
 }
 
-function render() {}
+function render() {
+  board.forEach(function (square, idx) {
+    if (square === 1) {
+      sqaureEls[idx].textContent = "X";
+    } else if (square === -1) {
+      sqaureEls[idx].textContent = "O";
+    } else {
+      sqaureEls[idx].textContent = "";
+    }
+  });
+
+  switch (winner) {
+    case "T":
+      messageEl.textContent = `It's a tie!`;
+      break;
+    case 1:
+      messageEl.textContent = `Player 1 wins!`;
+      break;
+    case -1:
+      messageEl.textContent = `Player 2 wins!`;
+      break;
+    default:
+      const playerTurn = turn === 1 ? "1" : "2";
+      messageEl.textContent = `It's Player ${playerTurn}'s turn!`;
+      break;
+  }
+}
